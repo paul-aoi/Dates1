120b4b151f662c558ed5e252f07c23abf3e4745a
paul-aoi <palu83xoo@yahoo.com>
2016-06-02 15:49:08 -0400

    Add files via upload

---
+++ Flipper.java
@@ -0,0 +1,28 @@
+package flipper;
+
+import java.util.Scanner;
+
+public class Flipper {
+
+	public static void main(String[] args) {
+
+		
+		Scanner ng = new Scanner(System.in); 
+		System.out.println("Please enter a number to reverse");
+		int f= ng.nextInt(); 
+		
+		int reverse = 0;
+				
+		int remainder;
+		
+		while(f!=0){
+			remainder=f%10;
+			reverse=reverse*10+remainder;
+			f=f/10;
+		
+		}
+		
+		System.out.println(reverse);
+	}
+
+}
